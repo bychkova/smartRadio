8e55a7f94d6ecabf784312023af57d5e807f390b
Maria Bychkova <mar.byckova@gmail.com>
2022-04-22 14:14:22 +0300

    Added Tests

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,37 +1,80 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-    @Test
-    void sholdSetStation(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataSetStation.csv")
+    void shouldSetStation(String testName, int station, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(station);
 
+        int actual = radio.getStation();
+
+        assertEquals(actual, expected);
     }
 
-    @Test
-    void sholdSetVolume(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataNextStation.csv")
+    void shouldNextStation(String testName, int station, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(station);
+        radio.nextStation();
 
+        int actual = radio.getStation();
+
+        assertEquals(actual, expected);
     }
 
-    @Test
-    void sholdNextStation(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataPrevStation.csv")
+    void shouldPrevStation(String testName, int station, int expected) {
+        Radio radio = new Radio();
+        radio.setStation(station);
+        radio.prevStation();
 
+        int actual = radio.getStation();
+
+        assertEquals(actual, expected);
     }
 
-    @Test
-    void sholdPrevStation(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataSetVolume.csv")
+    void shouldSetVolume(String testName, int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
 
+        int actual = radio.getVolume();
+
+        assertEquals(actual, expected);
     }
 
-    @Test
-    void sholdIncreaseVolume(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataIncreaseVolume.csv")
+    void shouldIncreaseVolume(String testName, int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
+        radio.increaseVolume();
 
+        int actual = radio.getVolume();
+
+        assertEquals(actual, expected);
     }
 
-    @Test
-    void sholdDecreaseVolume(){
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataDecreaseVolume.csv")
+    void shouldDecreaseVolume(String testName, int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
+        radio.decreaseVolume();
 
+        int actual = radio.getVolume();
+
+        assertEquals(actual, expected);
     }
 }
